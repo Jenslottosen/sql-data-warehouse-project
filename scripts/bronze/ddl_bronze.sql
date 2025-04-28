7c97763b5f0f89c8e6bdef2170b43f05c04bed3d
Jens Ottosen <95073893+Jenslottosen@users.noreply.github.com>
2025-04-28 14:12:37 +0200

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -56,8 +56,9 @@ GO
 IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_cust_az12;
 CREATE TABLE bronze.erp_cust_az12 (
-    cid   NVARCHAR(50),
-    cntry NVARCHAR(50)
+    cid    NVARCHAR(50),
+    bdate  DATE,
+    gen    NVARCHAR(50)
 );
 GO
 
